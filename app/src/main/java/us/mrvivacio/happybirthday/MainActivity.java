8986a751ac1bbd6553ac85c65df9fd523b5e3add
Vivek Bhookya <v.bhookya98@gmail.com>
2018-12-12 11:34:55 -0600

    We can screen for input

--- app/src/main/java/us/mrvivacio/happybirthday/MainActivity.java
+++ app/src/main/java/us/mrvivacio/happybirthday/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,23 +15,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String name;
-        String number;
-        String month;
-        String day;
-
         Button add = findViewById(R.id.b_Add);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                name = findViewById(R.id.et_Name).toString();
-//                number = findViewById(R.id.et_PhoneNum).toString();
-//                month = findViewById(R.id.et_Month).toString();
-//                day = findViewById(R.id.et_Day).toString();
+                // Thank you, https://developer.android.com/reference/android/widget/EditText
+                String name = ((TextView) findViewById(R.id.et_Name)).getText().toString();
+                String number = ((TextView) findViewById(R.id.et_PhoneNum)).getText().toString();
+                String month = ((TextView) findViewById(R.id.et_Month)).getText().toString();
+                String day = ((TextView) findViewById(R.id.et_Day)).getText().toString();
 
-                Toast.makeText(MainActivity.this, "sup dawg", Toast.LENGTH_SHORT).show();
+                // Screen for valid inputs
+                if (name.length() < 1) {
+                    myToast("Please add a name");
+                    return;
+                }
+
+                else if (number.length() < 1) {
+                    myToast("Please add a phone number");
+                    return;
+                }
+
+                else if (month.length() < 1) {
+                    myToast("Please add a month");
+                    return;
+                }
+
+                else if (day.length() < 1) {
+                    myToast("Please add a day");
+                    return;
+                }
+
+                // We have valid input, so add this person to sharedPref
+                else {
+//                    myToast("Good work: " + name + " -- " + number);
+
+                }
             }
         });
+    }
+
+    private void myToast(String msg) {
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
 }
